6cbd2ec859bab12cafa836608e0aabe4ace860f3
moyu <953009672@qq.com>
2021-08-01 18:00:32 +0800

    master1.0

--- src/main/java/edu/qust/Test01.java
+++ src/main/java/edu/qust/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("测试测试");
         System.out.println("hot-fix新增");
+        System.out.println("master新增");
     }
 
 }
